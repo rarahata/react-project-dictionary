4fb32aa5b686f8ead731efe705c6fa1fbb323372
rarahata <rei.arahata@gmail.com>
2022-02-17 17:03:55 +0000

    add axios

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function SearchEngine() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    alert(response.data[0].word);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    let apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/sunset";
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function updateKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="SearchEngine">
-      <form>
-        <div class="input-group rounded">
+      <form onChange={updateKeyword}>
+        <div className="input-group rounded">
           <input
             type="search"
-            class="form-control rounded"
+            className="form-control rounded"
             autoFocus
             placeholder="Search"
           />
-          <button className="btn">
-            <span class="group-text border-0">
-              <i class="fas fa-search"></i>
+          <button className="btn btn-primary" onClick={search}>
+            <span className="group-text border-0">
+              <i className="fas fa-search"></i>
             </span>
           </button>
         </div>
